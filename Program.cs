0fe879cca0058b79cc452983cf8ee7f5c446aa73
raminheydari83 <raminheydaripooyani83@gmail.com>
2025-10-31 23:02:20 +0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,28 @@
 
 class Program
 {
+    static void Main()using System;
+
+class Program
+{
     static void Main()
+    {
+        Console.Write("Enter a number (1-7): ");
+        int n = int.Parse(Console.ReadLine() ?? "0");
+
+        switch (n)
+        {
+            case 1: Console.WriteLine("Shanbe"); break;
+            case 2: Console.WriteLine("yek shanbe"); break;
+            case 3: Console.WriteLine("2shanbe"); break;
+            case 4: Console.WriteLine("3shanbe"); break;
+            case 5: Console.WriteLine("4shanbe"); break;
+            case 6: Console.WriteLine("5shanbe"); break;
+            case 7: Console.WriteLine("jome "); break;
+            default: Console.WriteLine("END"); break;
+        }
+    }
+}
     {
         Console.WriteLine("Letter E:");
         Console.WriteLine("#####");
@@ -26,3 +47,4 @@ class Program
         Console.WriteLine("#####");
     }
 }
+
