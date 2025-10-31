a6121b440ebb04225e33238b4219bc85f345d475
raminheydari83 <raminheydaripooyani83@gmail.com>
2025-10-31 22:56:53 +0330

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,19 +4,25 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Enter a number (1-7): ");
-        int n = int.Parse(Console.ReadLine() ?? "0");
+        Console.WriteLine("Letter E:");
+        Console.WriteLine("#####");
+        Console.WriteLine("#");
+        Console.WriteLine("#####");
+        Console.WriteLine("#");
+        Console.WriteLine("#####");
 
-        switch (n)
-        {
-            case 1: Console.WriteLine("Shanbe"); break;
-            case 2: Console.WriteLine("yek shanbe"); break;
-            case 3: Console.WriteLine("2shanbe"); break;
-            case 4: Console.WriteLine("3shanbe"); break;
-            case 5: Console.WriteLine("4shanbe"); break;
-            case 6: Console.WriteLine("5shanbe"); break;
-            case 7: Console.WriteLine("jome "); break;
-            default: Console.WriteLine("END"); break;
-        }
+        Console.WriteLine("\nLetter H:");
+        Console.WriteLine("#   #");
+        Console.WriteLine("#   #");
+        Console.WriteLine("#####");
+        Console.WriteLine("#   #");
+        Console.WriteLine("#   #");
+
+        Console.WriteLine("\nLetter I:");
+        Console.WriteLine("#####");
+        Console.WriteLine("  #  ");
+        Console.WriteLine("  #  ");
+        Console.WriteLine("  #  ");
+        Console.WriteLine("#####");
     }
 }
